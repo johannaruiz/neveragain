d4d45db42f2a74eb5b1ac5535c49fe013751994e
johannaruiz <johannaruizmn@gmail.com>
2011-05-06 21:06:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neveragain::Application.config.secret_token = 'a2af46adc08ff033c32c1e6edc66a784a3d62703bcefab8b9c8e1f11fc9eb0b6d0bf9fbd1caf6e2edf7ab6a9136ccde94d702dd3a633adc3064e798e40324188'
