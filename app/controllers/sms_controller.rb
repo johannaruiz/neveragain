dd8485ad6695c058136b8931cc877ca443d385aa
johannaruiz <johannaruizmn@gmail.com>
2011-05-06 23:44:09 -0500

    Adjusted area code to filter out country code

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,6 +1,6 @@
 class SmsController < ApplicationController
   def post
-    Post.create(:body => params[:Body], :area_code => params[:From][0,3] )
+    Post.create(:body => params[:Body], :area_code => params[:From][2,3] )
   end
 
 end
