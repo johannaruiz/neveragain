444e89a283561f68ddb269fa4a8317ed30f592fb
johannaruiz <johannaruizmn@gmail.com>
2011-05-06 22:32:32 -0500

    Added model association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110507021158) do
+ActiveRecord::Schema.define(:version => 20110507031028) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "area_code"
